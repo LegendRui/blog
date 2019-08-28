a1c2c68eae78e84cf936e1b21fd3681b13c41849
LegendRui <838433043@qq.com>
2019-08-28 20:09:33 +0800

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 using namespace std;
 
-struct A {
-	char a;
-	int b;
-	short c;
-	double e;
-};
-struct B {
-	char a;
-	short b;
-	int c;
-	float e;
-};
-int main()  {
-	cout << sizeof(A) << " " << sizeof (B);
+void Swap(int A[], int i, int j) {
+	int temp = A[i];
+	A[i] = A[j];
+	A[j] = temp;
+}
+
+void BubbleSort(int A[], int n) {
+	for (int j = 0; j < n - 1; ++j) {
+		for (int i = 0; i < n - 1 - j; ++i) {
+			if (A[i] > A[i + 1]) {
+				Swap(A, i, i + 1);
+			}
+		}
+	}
+}
+
+void CocktailSort(int A[], int n) {
+	int left = 0;
+	int right = n - 1;
+	while (left < right) {
+		for (int i = left; i < right; ++i) {
+			if (A[i] > A[i + 1])
+				Swap(A, i, i + 1);
+		}
+		right--;
+		for (int i = right; i > left; --i) {
+			if (A[i - 1] > A[i])
+				Swap(A, i - 1, i);
+		}
+		left++;
+	}
+}
+
+int main() {
+	int A[] = {6, 5, 3, 1, 8, 7, 2, 4};
+	int n = sizeof(A) / sizeof(int);
+	CocktailSort(A, n);
+	for (int i = 0; i < n; ++i) {
+		cout << A[i] << " ";
+	}
+	cout << endl;
 	return 0;
 }
