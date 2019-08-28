5ec9e6ff16585c2e33403e06a30724bb8a4b76ad
LegendRui <838433043@qq.com>
2019-08-28 20:30:02 +0800

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -34,10 +34,99 @@ void CocktailSort(int A[], int n) {
 	}
 }
 
+void SelectionSort(int A[], int n) {
+	for (int i = 0; i < n - 1; ++i)
+	{
+		int min = i;
+		for (int j = i + 1; j < n; j++) {
+			if (A[j] < A[min]) {
+				min = j;
+			}
+		}
+		if (min != i)
+			Swap(A, min, i);
+	}
+}
+
+void InsertionSort(int A[], int n) {
+	for (int i = 1; i < n; ++i) {
+		int get = A[i];
+		int j = i - 1;
+		while (j >= 0 && A[j] > get) {
+			A[j + 1] = A[j];
+			j--;
+		}
+		A[j + 1] = get;
+	}
+}
+
+void ShellSort(int A[], int n) {
+	int h = 0;
+	while (h <= n) {
+		h = 3 * h + 1;
+	}
+
+	while (h >= 1) {
+		for (int i = h; i < n; ++i) {
+			int j = i - h;
+			int get = A[i];
+			while (j >= 0 && A[j] > get) {
+				A[j + h] = A[j];
+				j = j - h;
+			}
+			A[j + h] = get;
+		}
+		h = (h - 1) / 3;
+	}
+}
+
+void Merge(int A[], int left, int mid, int right) {
+	int len = right - left + 1;
+	int *temp = new int[len];
+	int index = 0;
+	int i = left;
+	int j = mid + 1;
+	while (i <= mid && j <= right) {
+		temp[index++] = A[i] <= A[j] ? A[i++] : A[j++];
+	}
+	while (i <= mid) {
+		temp[index++] = A[i++];
+	}
+	while (j <= right) {
+		temp[index++] = A[j++];
+	}
+	for (int k = 0; k < len; ++k) {
+		A[left++] = temp[k];
+	}
+}
+
+void MergeSortRecursion(int A[], int left, int right) {
+	if (left == right)
+		return;
+	int mid = (left + right) / 2;
+	MergeSortRecursion(A, left, mid);
+	MergeSortRecursion(A, mid + 1, right);
+	Merge(A, left, mid, right);
+}
+
+void MergeSortIteration(int A[], int len) {
+	int left, mid, right;
+	for (int i = 1; i < len; i *= 2) {
+		left = 0;
+		while (left + i < len) {
+			mid = left + i - 1;
+			right = mid + i < len ? mid + i : len - 1;
+			Merge(A, left, mid, right);
+			left = right + 1;
+		}
+	}
+}
+
+
 int main() {
 	int A[] = {6, 5, 3, 1, 8, 7, 2, 4};
 	int n = sizeof(A) / sizeof(int);
-	CocktailSort(A, n);
+	MergeSortIteration(A, n);
 	for (int i = 0; i < n; ++i) {
 		cout << A[i] << " ";
 	}
